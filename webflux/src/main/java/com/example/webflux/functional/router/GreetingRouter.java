a42dae4cac7d6c6ffec6fac36849b72add2b3e04
Александр Плохой <alexplohoy@mail.ru>
2024-04-01 16:40:41 +0300

    add webflux

---
+++ webflux/src/main/java/com/example/webflux/functional/router/GreetingRouter.java
@@ -0,0 +1,24 @@
+package com.example.webflux.functional.router;
+
+import com.example.webflux.functional.handler.GreetingHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration(proxyBeanMethods = false)
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        final RequestPredicate helloRoute = RequestPredicates
+                .GET("/hello")
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON));
+        final RequestPredicate mainRoute = RequestPredicates
+                .GET("/");
+
+        return RouterFunctions
+                .route(helloRoute, greetingHandler::hello)
+                .andRoute(mainRoute, greetingHandler::render);
+    }
+}
